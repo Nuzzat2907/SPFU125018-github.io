cef7edcc15ea8590405d23317d5e8a7b6c23c178
Nuzzat2907 <spfu125018@mitvpu.ac.in>
2026-04-11 07:16:46 +0530

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-let index = 0;
-
-function moveSlide(step) {
-  const slides = document.querySelector('.slides');
-  const total = document.querySelectorAll('.slides img').length;
-
-  index += step;
-
-  if (index < 0) index = total - 1;
-  if (index >= total) index = 0;
-
-  slides.style.transform = `translateX(-${index * 220}px)`;
-}
+console.log("Portfolio Loaded Successfully ✅");
